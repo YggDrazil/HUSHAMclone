d8dce92a8cc4bd315b5fffeb4b48e8f97307b328
hmemar <husham.memar@husham.com>
2015-08-14 17:17:14 +0100

    Repo update

--- repository/plugin.video.haydargames/default.py
+++ repository/plugin.video.haydargames/default.py
@@ -14,7 +14,7 @@ import xbmcgui
 import xbmcaddon
 import xbmcplugin
 
-BASE_URL = 'http://iwaztv.com/?p=20'
+BASE_URL = 'http://www.gametest.dk/'
 PLAY_VIDEO_PATH = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s'
 PLAYLIST_PATH = 'plugin://plugin.video.youtube/user/stampylonghead/'
 PLAYLIST_PATH2 = 'plugin://plugin.video.youtube/user/stampylongnose/'
--- zip/plugin.video.ryanreggaendancehallmusic/default.py
+++ zip/plugin.video.ryanreggaendancehallmusic/default.py
@@ -14,7 +14,7 @@ import xbmcgui
 import xbmcaddon
 import xbmcplugin
 
-BASE_URL = 'http://iwaztv.com/?p=51'
+BASE_URL = 'http://www.gametest.dk/'
 PLAY_VIDEO_PATH = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s'
 PLAYLIST_PATH1 = 'plugin://plugin.video.youtube/user/AkamEntertainments/'
 PLAYLIST_PATH2 = 'plugin://plugin.video.youtube/user/TheReggaeselecta/'
