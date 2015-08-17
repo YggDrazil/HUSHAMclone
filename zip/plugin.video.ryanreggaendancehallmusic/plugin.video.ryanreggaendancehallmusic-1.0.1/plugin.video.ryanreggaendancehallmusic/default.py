d7d1cba43157d1491e892f7957c7ac62f6043ae2
hmemar <husham.memar@husham.com>
2015-08-17 02:01:23 +0100

    repo update and bug fix to ryan addon

--- zip/plugin.video.ryanreggaendancehallmusic/plugin.video.ryanreggaendancehallmusic-1.0.1/plugin.video.ryanreggaendancehallmusic/default.py
+++ zip/plugin.video.ryanreggaendancehallmusic/plugin.video.ryanreggaendancehallmusic-1.0.1/plugin.video.ryanreggaendancehallmusic/default.py
@@ -14,11 +14,11 @@ import xbmcgui
 import xbmcaddon
 import xbmcplugin
 
-BASE_URL = 'http://www.gametest.dk/'
+BASE_URL = 'http://iwaztv.com/?p=51'
 PLAY_VIDEO_PATH = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s'
-PLAYLIST_PATH = 'plugin://plugin.video.youtube/user/AkamEntertainments/'
+PLAYLIST_PATH1 = 'plugin://plugin.video.youtube/user/AkamEntertainments/'
 PLAYLIST_PATH2 = 'plugin://plugin.video.youtube/user/TheReggaeselecta/'
-PLAYLIST_PATH3 = 'plugin://plugin.video.youtube/channel/UC7_aGyrvwh5TBZvyleQ97pw/'
+PLAYLIST_PATH3 = 'plugin://plugin.video.youtube/user/UC7_aGyrvwh5TBZvyleQ97pw/'
 
 if __name__ == '__main__':
     ADDON = xbmcaddon.Addon()
@@ -35,15 +35,16 @@ if __name__ == '__main__':
             item = xbmcgui.ListItem('Reggae n Dancehall music',
                                     ADDON.getLocalizedString(30001),
                                     iconImage='https://yt3.ggpht.com/-wQ8woE0PkAM/AAAAAAAAAAI/AAAAAAAAAAA/V5jh5XF6NBQ/s100-c-k-no/photo.jpg')
-            xbmcplugin.addDirectoryItem(HANDLE, PLAYLIST_PATH, item, True)
-            item = xbmcgui.ListItem('The Reggae Selecta',
+            xbmcplugin.addDirectoryItem(HANDLE, PLAYLIST_PATH1, item, True)
+           item = xbmcgui.ListItem('The Reggae Selecta',
                                     ADDON.getLocalizedString(30001),
                                     iconImage='https://yt3.ggpht.com/-EvhOxI8WdWg/AAAAAAAAAAI/AAAAAAAAAAA/e8X3nNDSpvY/s100-c-k-no/photo.jpg')
-            xbmcplugin.addDirectoryItem(HANDLE, PLAYLIST_PATH2, item, True)  
-            item = xbmcgui.ListItem('Lloyd Williams',
+            xbmcplugin.addDirectoryItem(HANDLE, PLAYLIST_PATH2, item, True)
+           item = xbmcgui.ListItem('Lloyd Williams',
                                     ADDON.getLocalizedString(30001),
                                     iconImage='https://yt3.ggpht.com/-pYpEOEE_IS4/AAAAAAAAAAI/AAAAAAAAAAA/t70MqZ9IxSQ/s100-c-k-no/photo.jpg')
-            xbmcplugin.addDirectoryItem(HANDLE, PLAYLIST_PATH3, item, True) 			
+            xbmcplugin.addDirectoryItem(HANDLE, PLAYLIST_PATH3, item, True)
+            		            		            		
         xbmcplugin.endOfDirectory(HANDLE)
     except:
         buggalo.onExceptionRaised()
