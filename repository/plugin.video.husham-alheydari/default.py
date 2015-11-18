1dfc8aef6c2a4f4f60bbdcf95556ed96983760fd
husham memar <website@husham.com>
2015-11-18 16:07:10 +0000

    update

--- repository/plugin.video.husham-alheydari/default.py
+++ repository/plugin.video.husham-alheydari/default.py
@@ -21,6 +21,9 @@ local = xbmcaddon.Addon(id=addonID)
 icon = local.getAddonInfo('icon')
 
 YOUTUBE_CHANNEL_ID_1 = "UCFj9KQcceK3XduMOVNyXrVg"
+YOUTUBE_CHANNEL_ID_2 = "UCVZNS89ZdukOh26qBnM-S_g"
+YOUTUBE_CHANNEL_ID_3 = "UCrgJRoRy37qzHOYLz7u6H4A"
+YOUTUBE_CHANNEL_ID_4 = "UCBDsHHOrbdlrTOgkRvhYF5g"
 
 # Entry point
 def run():
@@ -47,4 +50,23 @@ def main_list(params):
         url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_1+"/",
         thumbnail="https://yt3.ggpht.com/-yTO8iBROuII/AAAAAAAAAAI/AAAAAAAAAAA/ltUtz3Y095Q/s100-c-k-no/photo.jpg",
         folder=True )
+    plugintools.add_item( 
+        #action="", 
+        title="اية الله السيد كمال الحيدري",
+        url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_2+"/",
+        thumbnail="https://yt3.ggpht.com/-wMd67Z6lN6Y/AAAAAAAAAAI/AAAAAAAAAAA/lwFbCFdfQHg/s100-c-k-no/photo.jpg",
+        folder=True )
+    plugintools.add_item( 
+        #action="", 
+        title="alhaydari-tube",
+        url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_3+"/",
+        thumbnail="https://yt3.ggpht.com/-TiFlfrdhBgg/AAAAAAAAAAI/AAAAAAAAAAA/jdYf3F7R7YY/s100-c-k-no/photo.jpg",
+        folder=True )
+    plugintools.add_item( 
+        #action="", 
+        title="مقتطفات مهمة من بحوث السيد كمال الحيدري",
+        url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID_4+"/",
+        thumbnail="https://yt3.ggpht.com/-JktRrJlrQQ8/AAAAAAAAAAI/AAAAAAAAAAA/1PE5HcYxao4/s100-c-k-no/photo.jpg",
+        folder=True )
+
 run()
