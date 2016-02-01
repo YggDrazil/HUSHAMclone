ec2c54f8a464ee41a59de990252fbadc48155712
husham memar <website@husham.com>
2016-02-01 01:18:39 +0000

    update

--- zip/plugin.video.hushamaddonwizard/default.py
+++ zip/plugin.video.hushamaddonwizard/default.py
@@ -22,7 +22,7 @@ PATH = "Husham Wizard"
 
     
 def CATEGORIES():
-    link = OPEN_URL('https://archive.org/download/sourcefile/sourcefile.txt').replace('\n','').replace('\r','')
+    link = OPEN_URL('http://pastebin.com/raw/ua9mLPHi').replace('\n','').replace('\r','')
     match = re.compile('name="(.+?)".+?rl="(.+?)".+?mg="(.+?)".+?anart="(.+?)".+?escription="(.+?)"').findall(link)
     for name,url,iconimage,fanart,description in match:
         addDir(name,url,1,iconimage,fanart,description)
