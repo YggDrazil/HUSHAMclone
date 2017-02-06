41e9fb20c8058c1d829ca3c1b23cac723fa46019
husham memar <website@husham.com>
2017-02-06 22:06:36 +0000

    new commit

--- repository/plugin.video.news12/addon.py
+++ repository/plugin.video.news12/addon.py
@@ -1,1 +1,117 @@
-exec("import re;import base64");exec((lambda p,y:(lambda o,b,f:re.sub(o,b,f))(r"([0-9a-f]+)",lambda m:p(m,y),base64.b64decode("NTUoImU4IGQ5O2U4IDIzIik7NTUoKDcgMTExLDEwYzooNyAxMWQsYixmOmQ5LmViKDExZCxiLGYpKSgxMzMiKFswLTlhLWZdKykiLDcgMTIzOjExMSgxMjMsMTBjKSwyMy4zZCgiMi8xMzAvMTJkLzQvMTBhPSIpKSkoNyBhLGI6YltmNigiMTE0IithLmI0KDEpLDE2KV0sIjhlfDF8YmR8M3w2OXw4Ynw2fDExfDh8OXxhfGJ8ZGN8NDF8ZDF8ODl8MTB8Njd8MTJ8OGF8OTZ8Y3wxMjZ8MTh8ZjV8YTd8MzR8MmF8MmV8ZXwxMDh8MTI3fDEzfDEyOHwxOXwxNHxlM3xjYnxjY3xiN3xhM3w3Nnw0M3w0OXwyMXxlN3wzN3xmYXwxMDV8MTAzfDMzfDk0fDNjfDZlfDE3fGE1fDExOXxiMXw0Mnw1OXwxMGZ8MWR8YzZ8NmF8MTE2fDEyNHwxZnw1fDQ2fDEzMnwxMmN8MTI5fGM5fDEwNHwyMnwyYnwyY3wyZnw1ZXw3MXw2ZHxiM3w3Y3xmMXwzYXw5Y3w5ZXxiMnxkfGQ4fGNkfDRkfDU0fGQ1fDFhfGEyfDFjfDFifDk1fDVjfDU3fDU4fDc3fDVkfDYzfDFlfGRmfDcwfDczfDc4fDIwfDc5fDgwfDgyfDgzfDdifDdmfDg0fDI0fDkyfDkzfDI1fDI2fDk4fDI3fDI4fDI5fDJkfDMwfDMxfGE2fDM2fDEwZXwzNXwzOHwzYnxmMnxmYnwxMDd8M2V8Mzl8M2Z8NDB8NDR8NTN8YmJ8NDV8YmZ8NGZ8NDh8YzV8NGF8NGN8Yjh8NTB8ZDZ8NTF8NGJ8NTJ8MTBkfGRlfDRlfDQ3fDU2fDVhfDVifDVmfDYxfDYyfDY0fDY2fDZjfDY4fDZifDZmfDY1fDcyfDYwfDc0fDc1fDhkfDdhfDdkfDdlfGU1fDgxfDg1fDg3fDg4fGY0fGJlfDhmfDhjfDExYXxmY3wxMWJ8OTB8OTF8ODZ8OTd8MTFmfDk5fDlifDEyMXw5ZHxhY3w5ZnxhMHxhMXxhNHxhOHxhOXxhYXxhYnxhZXxhZnxiMHxiNXxiNnxiOXxjZnwxMjJ8Yzd8YzB8YzF8ZDR8Y2F8YzN8YzR8MTEzfGJhfDExNXxjOHxjMnwxMzF8Y2V8ZDB8ZDJ8ZDd8ZGF8ZGJ8YmN8ZDN8ZTB8ZTF8ZTJ8ZTl8ZWF8ZWR8ZWV8MTJlfGVmfGYwfGYzfGY3fGY4fGY5fGVjfDEyMHxmZHxlNHxmZXxmZnwxMDB8MTAyfDEwMXwxMDZ8ZTZ8MTA5fDEwYnwxMmF8MTEwfDExN3wxMTh8MTFjfDExZXwxMTJ8ZTh8MTI1fGRkfDEyYnwxNXwxMmZ8MzIiLmFkKCJ8IikpKQ==")))(lambda a,b:b[int("0x"+a.group(1),16)],"0|1|IyEvMTAzLzEwOS9jYQojIC0qLSBiYjogZjUtOCAtKi0KIwojICAgIDk5IDUwIGMxIGU4IDk4OiBmNyAxMGEgN2EgYzMgMTA3L2NiIGI0CiMgICAgYzMgZDggNDkgZDUgOWYgNDkgODkgNGYgNTIgM2IgMTEwIDg2IDExNAojICAgIDQ5IGU0IDllIDgzLCBjNCA2NSAzIDlmIDQ5IDNiLCBjYgojICAgICgxMTEgZWYgYzYpIDEwNiBkMSA2NS4KIwojICAgIDk5IDUwIGMxIDdmIDg0IDQ5IGRiIGVkIGMzIGUwIGJlIGM3LAojICAgIGY4IGFmIDEwYiA5MjsgYjEgZTYgNDkgYTUgOTQgOWYKIyAgICA2MSBjYiBiMiAxMDUgMTE4IDgwIGFlLiAgMTAxIDQ5CiMgICAgODkgNGYgNTIgM2IgODggZjIgYTkuCiMKIyAgICAxMDggYmMgZWUgOWMgYSBlOSA5ZiA0OSA4OSA0ZiA1MiAzYgojICAgIGNmIGRlIGVjIDUwLiAxMGYgZmIsIDEwYyA8MTE3Oi8vMTA0LmZjLmZlLzlkLz4uCiMKCmUzIDg1IDExNSA3MAoKCjRlID0gewoJJzkxJzogZDIsCgknM2YnOiBkNiwKCSc4Zic6IGQ3LAoJCgp9CgozZCA9ICgKCXsKCQknMjgnOiAnMjcgMTIgOTMgNmYnLAoJCSdjJzogJzAuMTgnLAoJCScxMWInOiAoJzI2Oi8vMTAyLTIwLjM4LjJmLjNjLjJkLzUvMTAyLTIwIGQ9MTAyLTIwIDE0PTExNzovLzMyLjAuMjQvNDEvMjUvM2EvNi45LzNhLjE5LjMxIDMwPTUvMTAyLTIwIDExPTExNzovLzMyLjAuMjQvZS9lLTEyLWEwLTcyLTZhLWYtMS42Mz80MD0xJwoJCQkJCSAgICcgNmE9MiA2Mj0yIDQ9MTAnKSwKCX0sCgl7CgkJJzI4JzogJzI3IDEyIDkzIDZmIDI5JywKCQknYyc6ICcwLjE4JywKCQknMTFiJzogKCcyNjovLzEwMi0xNy4zOC4yZi4zYy4yZC81LzEwMi0xNy8xMDItMTcgZD0xMDItMTcgMTQ9MTE3Oi8vMzIuMC4yNC80MS8yNS8zYS82LjkvM2EuMTkuMzEgMzA9NS8xMDItMTcgMTE9MTE3Oi8vMzIuMC4yNC9lL2UtMTItYTAtNzItNmEtZi0xLjYzPzQwPTEnCgkJCQkJICAgJyA2YT0yIDYyPTIgND0xMCcpLAoJfSwKCXsKCQknMjgnOiAnMjcgMTIgODInLAoJCSdjJzogJzAuMTgnLAoJCScxMWInOiAoJzI2Oi8vMTAyLTFlLjM4LjJmLjNjLjJkLzUvMTAyLTFlIGQ9MTAyLTFlIDE0PTExNzovLzM3LjAuMjQvNDEvMjUvM2EvNi45LzNhLjE5LjMxIDMwPTUvMTAyLTFlIDExPTExNzovLzM3LjAuMjQvZS9lLTEyLTM3LTZhLWYtMS42Yj80MD0xIDZhPTIgNjI9MiA0PTEwJwoJCQkJCSAgICcgNmE9MiA2Mj0yIDQ9MTAnKSwKCX0sCgl7CgkJJzI4JzogJzI3IDEyIDgyIDI5JywKCQknYyc6ICcwLjE4JywKCQknMTFiJzogKCcyNjovLzEwMi0xMTkuMzguMmYuM2MuMmQvNS8xMDItMTE5LzEwMi0xMTkgZD0xMDItMTE5IDE0PTExNzovLzM3LjAuMjQvNDEvMjUvM2EvNi45LzNhLjE5LjMxIDMwPTUvMTAyLTExOSAxMT0xMTc6Ly8zNy4wLjI0L2UvZS0xMi0zNy02YS1mLTEuNmI|3|NDA9MSA2YT0yIDYyPTIgND0xMCcKCQkJCQkgICAnIDZhPTIgNjI9MiA0PTEwJyksCgl9LAoJewoJCScyOCc6ICcyNyAxMiA1YiAyOScsCgkJJ2MnOiAnMC4xOCcsCgkJJzExYic6ICgnMjY6Ly8xMDItMTE5LjM4LjJmLjNjLjJkLzUvMTAyLTExOS8xMDItMTE5IGQ9MTAyLTExOSAxND0xMTc6Ly8yYi4wLjI0LzQxLzI1LzNhLzYuOS8zYS4xOS4zMSAzMD01LzEwMi0xMTkgMTE9MTE3Oi8vMmIuMC4yNC9lL2UtMTItMmItNmEtZi0xLjY3PzQwPTEgNmE9MiA2Mj0yIDQ9MTAnCgkJCQkJICAgJyA2YT0yIDYyPTIgND0xMCcpLAoJfSwKCXsKCQknMjgnOiAnZmQgNWQnLAoJCSdjJzogJzAuMTgnLAoJCScxMWInOiAoJzExNzovL2E4LmIwLjI0LzEwZS84YkBiNy82ZS1iLjU5JyksCgl9LAoJewoJCScyOCc6ICc4NycsCgkJJ2MnOiAnMC4xOCcsCgkJJzExYic6ICgnMTE3Oi8vZGQuYmYuOGQuMjQvZjYvMTE2LzU2LjU5P2I|UCRrW0ddrbFnJCbyZqHHv4KQ|6|lambda|8|9|a|b|b3e3010000000000|5ko_eiXAupbDfxWw|bce3010000000000|f|10|b8e3010000000000|12|b4e3010000000000|bbe3010000000000|afe3010000000000|16|YOUTUBE_CHANNELS|aee3010000000000|b9e3010000000000|Hushamallsports|MERCHANTABILITY|STATIC_STREAMS2|STATIC_STREAMS|show_root_menu|show_channels|index_1200_av|hudsonvalley|show_streams|base64|OafjHESJciNw|getAddonInfo|redistribute|is_playable|twelvedottv|UC_DlTqJhRV|westchester|Westchester|YOUTUBE_URL|distributed|connecticut|Connecticut|PARTICULAR|get_string|stream_url|longisland|channel_id|xbmcswift2|Foundation|string_id|published|streaming|thumbnail|Bloomberg|newjersey|b64decode|abc_live4|bloomberg|resources|playpath|jwplayer|hmemar22|streams2|WARRANTY|get_logo|__main__|euronews|brooklyn|channels|licenses|software|Brooklyn|playlist|warranty|akamaihd|received|Software|__name__|addon_id|exec|NASA_101|3925819|version|License|6540154|implied|3923191|3925389|STRINGS|warning|without|UCupvZG|abclive|3925523|details|WITHOUT|ustream|pageUrl|English|timeout|streams|youtube|missing|program|channel|PURPOSE|3924984|General|abcnews|url_for|FITNESS|special|Weather|3925702|3924903|Island|nasatv|jersey|Public|String|136330|Valley|Plugin|Events|valley|island|iphone|_addon|option|coding|should|stream|plugin|vxedge|319270|modify|news12|videos|either|addons|hudson|JERSEY|return|swfVfy|swfUrl|useful|Hudson|husham|9a|python|finish|hmemar|master|media|along|Links|route|title|later|bronx|Bronx|flash|30001|video|event|terms|Lists|split|30100|30101|under|items|githubusercontent|label|group|https|True|News|This|hope|even|page|Live|true|be|Long|IPTV|will|copy|from|Free|text|name|with|free|user|else|libs|rtmp|path|info|home|this|news|that|more|WXII|NEWS|NASA|have|m3u8|re|your|WDEF|logo|http|long|live|Only|uhls|utf|com|www|new|ANY|net|import|btv|you|sub|See|but|raw|not|gnu|def|for|ABC|CNN|jpg|int|org|m3u|run|cdn|GNU|NEW|usr|FOR|any|and|bin|You|swf|the|app|can|log|b5e3010000000000|see|YioxMWEkJyksCgl9LAoJewoJCScyOCc6ICc5YiBlNycsCgkJJ2MnOiAnMC4xOCcsCgkJJzExYic6ICgnMTE3Oi8vYjUtMTEzLjlhLjJkLzEwZS9hM0BjMC81Ni41OScpLAoJfSwKCXsKCQknMjgnOiAnOWIgZTcgMTBkJywKCQknYyc6ICcwLjE4JywKCQknMTFiJzogKCcxMTc6Ly83NS04Yy5hYS4xMTIvZjQvYTQvM2YvNmEvNzUvYTEuNTknKSwKCX0sCikKCgo2MCA9ICgKCXsKCQknMjgnOiAnYjkgZGYgZjMgZDAnLAoJCSdjJzogJzVlLjE4JywKCQknMTFiJzogKCdkOTovL2Y5LjU3LjI0L2NjL2M5LjI0LzU2LycKCQkJCQkgICAnY2QvZDQuZmYnKSwKCX0sCikKCgozNiA9ICgKCXsKCQknM2UnOiAnZTEgMTIgNWQnLAoJCSdjJzogJzAuMTgnLAoJCScxYSc6ICc0NScsCgkJJzQ4JzogJzJhJywKCX0sIAoJewoJCSczZSc6ICdmMCAyNyAxMicsCgkJJ2MnOiAnMC4xOCcsCgkJJzFhJzogJzQ2JywKCQknNDgnOiAnMmEnLAoJfSwgCgl7CgkJJzNlJzogJzdkJywKCQknYyc6ICcwLjE4JywKCQknMWEnOiAnN2UtNzYnLAoJCSc0OCc6ICcyYScsCgl9LCAKCXsKCQknM2UnOiAnYmQnLAoJCSdjJzogJzAuMTgnLAoJCScxYSc6ICdhNy01OCcsCgkJJzQ4JzogJzJhJywKCX0sIAoJewoJCSczZSc6ICdiZCBmMScsCgkJJ2MnOiAnMC4xOCcsCgkJJzFhJzogJzQzJywKCQknNDgnOiAnMmEnLAoJfSwgCgl7CgkJJzNlJzogJzk1ICg4NCBhYyknLAoJCSdjJzogJzAuMTgnLAoJCScxYSc6ICc0NycsCgkJJzQ4JzogJzJhJywKCX0sICAKKQoKCgo0YyA9JzEzOi8vMTMuZDMuYWQvMzUvJWVhLz85MT0xJwoKMTMgPSA3MCgpCgoKQDEzLjVmKCcvJykKNTMgNjkoKToKCTM5ID0gWwoJCXsnNTEnOiBmYSgnM2YnKSwKCQkgJzVhJzogMTMuNmMoJzRhJyl9LAoJCSB7JzUxJzogZmEoJ2M4JyksCgkJICc1YSc6IDEzLjZjKCc0MicpfSwKCgldCgkzMyAxMy41NSgzOSkKCgpAMTMuNWYoJy8zZi8nKQo1MyA0YSgpOgoJMzkgPSBbewoJCSc1MSc6IGZbJzI4J10sCgkJJzU0JzogNDQoZlsnYyddKSwKCQknNWEnOiBmWycxMWInXSwKCQknN2MnOiBkYSwKCX0gODggZiA4NCAzZF0KCTMzIDEzLjU1KDM5KQoKCQoJCgpAMTMuNWYoJy85Ny8nKQo1MyA0MigpOgoJMzkgPSBbewoJCSc1MSc6IDM1WyczZSddLAoJCSc1NCc6IDQ0KDM1WydjJ10pLAoJCSc1YSc6IDRjICUgMzVbJzFhJ10sCgl9IDg4IDM1IDg0IDM2XQoJMzMgMTMuNTUoMzkpCgo1MyA0NChjKToKCTVjID0gMTMuYmEuNzkoJzM4JykKCTMzICdiMzovL2RjL2M1LyVlYS84ZS9jZS8lZWEnICUgKDVjLCBjKQoKCjUzIGZhKDJlKToKCWU1IDJlIDg0IDRlOgoJCTMzIDEzLjgxKDRlWzJlXSkKCWUyOgoJCTEzLjhhLmE2KCdiNiBjMSBhYjogJWVhJyAlIDJlKQoJCTMzIDJlCgoKNTMgOGEoOTYpOgoJMTMuOGEuZWIoOTYpCgplNSA5MCA9PSAnYTInOgoJMTMuMTAwKCk|HD|y|of|in|cv|If|p|by|if|0x|TV|qr|as|at|id|is|it|tv|o|lh|Videos|fs|or|cdn3|m|js|us|b6e3010000000000|ace3010000000000|bae3010000000000|UCSrZ3UV4jOidv8ppoVuvW9Q|i|A|UCMp6dYTbPOrKFHMH6rGcp2g|NDA9MSA2YT0yIDYyPTIgND0xMCcKCQkJCQkgICAnIDZhPTIgNjI9MiA0PTEwJyksCgl9LAoJewoJCScyOCc6ICcyNyAxMiA3YiA3NCcsCgkJJ2MnOiAnMC4xOCcsCgkJJzExYic6ICgnMjY6Ly8xMDItMjEuMzguMmYuM2MuMmQvNS8xMDItMjEgZD0xMDItMjEgMTQ9MTE3Oi8vMmMuMC4yNC80MS8yNS8zYS82LjkvM2EuMTkuMzEgMzA9NS8xMDItMjEgMTE9MTE3Oi8vMmMuMC4yNC9lL2UtMTItNzctNzEtNmEtZi0xLjY2PzQwPTEgNmE9MiA2Mj0yIDQ9MTAnCgkJCQkJICAgJyA2YT0yIDYyPTIgND0xMCcpLAoJfSwKCXsKCQknMjgnOiAnMjcgMTIgN2IgNzQgMjknLAoJCSdjJzogJzAuMTgnLAoJCScxMWInOiAoJzI2Oi8vMTAyLTcuMzguMmYuM2MuMmQvNS8xMDItNy8xMDItNyBkPTEwMi03IDE0PTExNzovLzJjLjAuMjQvNDEvMjUvM2EvNi45LzNhLjE5LjMxIDMwPTUvMTAyLTcgMTE9MTE3Oi8vMmMuMC4yNC9lL2UtMTItNzctNzEtNmEtZi0xLjY2PzQwPTEgNmE9MiA2Mj0yIDQ9MTAnCgkJCQkJICAgJyA2YT0yIDYyPTIgND0xMCcpLAoJfSwKCXsKCQknMjgnOiAnNWQgMTIgYzIgNzgnLAoJCSdjJzogJzAuMTgnLAoJCScxMWInOiAoJzI2Oi8vMTAyLTIyLjM4LjJmLjNjLjJkLzUvMTAyLTIyIGQ9MTAyLTIyIDE0PTExNzovLzM0LjAuMjQvNDEvMjUvM2EvNi45LzNhLjE5LjMxIDMwPTUvMTAyLTIyIDExPTExNzovLzM0LjAuMjQvZS9lLTEyLWI4LTczLTZhLWYtMS42ND80MD0xIDZhPTIgNjI9MiA0PTEwJwoJCQkJCSAgICcgNmE9MiA2Mj0yIDQ9MTAnKSwKCX0sCgl7CgkJJzI4JzogJzVkIDEyIGMyIDc4IDI5JywKCQknYyc6ICcwLjE4JywKCQknMTFiJzogKCcyNjovLzEwMi0xNS4zOC4yZi4zYy4yZC81LzEwMi0xNS8xMDItMTUgZD0xMDItMTUgMTQ9MTE3Oi8vMzQuMC4yNC80MS8yNS8zYS82LjkvM2EuMTkuMzEgMzA9NS8xMDItMTUgMTE9MTE3Oi8vMzQuMC4yNC9lL2UtMTItYjgtNzMtNmEtZi0xLjY0PzQwPTEgNmE9MiA2Mj0yIDQ9MTAnCgkJCQkJICAgJyA2YT0yIDYyPTIgND0xMCcpLAoJfSwKCXsKCQknMjgnOiAnMjcgMTIgNGInLAoJCSdjJzogJzAuMTgnLAoJCScxMWInOiAoJzI2Oi8vMTAyLTFkLjM4LjJmLjNjLjJkLzUvMTAyLTFkIGQ9MTAyLTFkIDE0PTExNzovLzFiLjAuMjQvNDEvMjUvM2EvNi45LzNhLjE5LjMxIDMwPTUvMTAyLTFkIDExPTExNzovLzFiLjAuMjQvZS9lLTEyLTFiLTZhLWYtMS42ZD80MD0xIDZhPTIgNjI9MiA0PTEwJwoJCQkJCSAgICcgNmE9MiA2Mj0yIDQ9MTAnKSwKCX0sCgl7CgkJJzI4JzogJzI3IDEyIDRiIDI5JywKCQknYyc6ICcwLjE4JywKCQknMTFiJzogKCcyNjovLzEwMi03LjM4LjJmLjNjLjJkLzUvMTAyLTcvMTAyLTcgZD0xMDItNyAxND0xMTc6Ly8xYi4wLjI0LzQxLzI1LzNhLzYuOS8zYS4xOS4zMSAzMD01LzEwMi03IDExPTExNzovLzFiLjAuMjQvZS9lLTEyLTFiLTZhLWYtMS42ZD80MD0xIDZhPTIgNjI9MiA0PTEwJwoJCQkJCSAgICcgNmE9MiA2Mj0yIDQ9MTAnKSwKCX0sCgl7CgkJJzI4JzogJzI3IDEyIDViJywKCQknYyc6ICcwLjE4JywKCQknMTFiJzogKCcyNjovLzEwMi0xZi4zOC4yZi4zYy4yZC81LzEwMi0xZiBkPTEwMi0xZiAxND0xMTc6Ly8yYi4wLjI0LzQxLzI1LzNhLzYuOS8zYS4xOS4zMSAzMD01LzEwMi0xZiAxMT0xMTc6Ly8yYi4wLjI0L2UvZS0xMi0yYi02YS1mLTEuNjc|_|t|NDA9MSA2YT0yIDYyPTIgND0xMCcKCQkJCQkgICAnIDZhPTIgNjI9MiA0PTEwJyksCgl9LAoJewoJCScyOCc6ICcyNyAxMiA0ZCcsCgkJJ2MnOiAnMC4xOCcsCgkJJzExYic6ICgnMjY6Ly8xMDItMjMuMzguMmYuM2MuMmQvNS8xMDItMjMgZD0xMDItMjMgMTQ9MTE3Oi8vMWMuMC4yNC80MS8yNS8zYS82LjkvM2EuMTkuMzEgMzA9NS8xMDItMjMgMTE9MTE3Oi8vMWMuMC4yNC9lL2UtMTItMWMtNmEtZi0xLjY4PzQwPTEgNmE9MiA2Mj0yIDQ9MTAnCgkJCQkJICAgJyA2YT0yIDYyPTIgND0xMCcpLAoJfSwKCXsKCQknMjgnOiAnMjcgMTIgNGQgMjknLAoJCSdjJzogJzAuMTgnLAoJCScxMWInOiAoJzI2Oi8vMTAyLTE2LjM4LjJmLjNjLjJkLzUvMTAyLTE2LzEwMi0xNiBkPTEwMi0xNiAxND0xMTc6Ly8xYy4wLjI0LzQxLzI1LzNhLzYuOS8zYS4xOS4zMSAzMD01LzEwMi0xNiAxMT0xMTc6Ly8xYy4wLjI0L2UvZS0xMi0xYy02YS1mLTEuNjg|s|UC0ZsHHC_frpcqneDpMiteCQ|r".split("|")))
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+exec("import re;import base64");exec((lambda p,y:(lambda o,b,f:re.sub(o,b,f))(r"([0-9a-f]+)",lambda m:p(m,y),base64.b64decode("NTYgMmYgNDcgM2YKCgozYiA9IHsKCSc1MCc6IDRjLAoJJzI0JzogNGUsCgknMzQnOiA0ZCwKCQoKfQoKMjUgPSAoCgl7CgkJJzQnOiAnMTAgMTIgMzcgMjgnLAoJCSc3JzogJzEuOCcsCgkJJzAnOiAoJzk6Ly82LWUuMTkuMTcuMTguMTYvNS82LWUvNi1lJyksCgl9LAoJewoJCSc0JzogJzEwIDEyIDM3IDI4IGEnLAoJCSc3JzogJzEuOCcsCgkJJzAnOiAoJzk6Ly82LTE0LjE5LjE3LjE4LjE2LzUvNi0xNC82LTE0JyksCgl9LAoJewoJCSc0JzogJzEwIDEyIDJlJywKCQknNyc6ICcxLjgnLAoJCScwJzogKCc5Oi8vNi0xNS4xOS4xNy4xOC4xNi81LzYtMTUvNi0xNScpLAoJfSwKCXsKCQknNCc6ICcxMCAxMiAyZSBhJywKCQknNyc6ICcxLjgnLAoJCScwJzogKCc5Oi8vNi0yLjE5LjE3LjE4LjE2LzUvNi0yLzYtMicpLAoJfSwKCXsKCQknNCc6ICcxMCAxMiAxYicsCgkJJzcnOiAnMS44JywKCQknMCc6ICgnOTovLzYtMWEuMTkuMTcuMTguMTYvNS82LTFhLzYtMWEnKSwKCX0sCgl7CgkJJzQnOiAnMTAgMTIgMWIgYScsCgkJJzcnOiAnMS44JywKCQknMCc6ICgnOTovLzYtZi4xOS4xNy4xOC4xNi81LzYtZi82LWYnKSwKCX0sCgl7CgkJJzQnOiAnMTAgMTIgMmEgMmInLAoJCSc3JzogJzEuOCcsCgkJJzAnOiAoJzk6Ly82LWQuMTkuMTcuMTguMTYvNS82LWQvNi1kJyksCgl9LAoJewoJCSc0JzogJzEwIDEyIDJhIDJiIGEnLAoJCSc3JzogJzEuOCcsCgkJJzAnOiAoJzk6Ly82LTMuMTkuMTcuMTguMTYvNS82LTMvNi0zJyksCgl9LAoJewoJCSc0JzogJzI3IDEyIDQzIDJkJywKCQknNyc6ICcxLjgnLAoJCScwJzogKCc5Oi8vNi01YS4xOS4xNy4xOC4xNi81LzYtNWEvNi01YScpLAoJfSwKCXsKCQknNCc6ICcyNyAxMiA0MyAyZCBhJywKCQknNyc6ICcxLjgnLAoJCScwJzogKCc5Oi8vNi0xMS4xOS4xNy4xOC4xNi81LzYtMTEvNi0xMScpLAoJfSwKCXsKCQknNCc6ICcxMCAxMiAxYycsCgkJJzcnOiAnMS44JywKCQknMCc6ICgnOTovLzYtYy4xOS4xNy4xOC4xNi81LzYtYy82LWMnKSwKCX0sCgl7CgkJJzQnOiAnMTAgMTIgMWMgYScsCgkJJzcnOiAnMS44JywKCQknMCc6ICgnOTovLzYtMy4xOS4xNy4xOC4xNi81LzYtMy82LTMnKSwKCX0sCgl7CgkJJzQnOiAnMTAgMTIgMjAnLAoJCSc3JzogJzEuOCcsCgkJJzAnOiAoJzk6Ly82LTEzLjE5LjE3LjE4LjE2LzUvNi0xMy82LTEzJyksCgl9LAoJewoJCSc0JzogJzEwIDEyIDIwIGEnLAoJCSc3JzogJzEuOCcsCgkJJzAnOiAoJzk6Ly82LTIuMTkuMTcuMTguMTYvNS82LTIvNi0yJyksCgl9LAoJewoJCSc0JzogJzU5IDI3JywKCQknNyc6ICcxLjgnLAoJCScwJzogKCcxZjovLzNkLjNjLjI5LzVkLzMyQDQ0LzI2LWIuMjEnKSwKCX0sCgl7CgkJJzQnOiAnMzEnLAoJCSc3JzogJzEuOCcsCgkJJzAnOiAoJzFmOi8vNTUuNDAuMzAuMjkvNTgvNWUvMWQuMjE/Yj9iKjYyJCcpLAoJfSwKCXsKCQknNCc6ICczNSA1MycsCgkJJzcnOiAnMS44JywKCQknMCc6ICgnMWY6Ly80NS02MC4zNi4xNi81ZC8zOUA0Mi8xZC4yMScpLAoJfSwKCXsKCQknNCc6ICczNSA1MyA1ZicsCgkJJzcnOiAnMS44JywKCQknMCc6ICgnMWY6Ly8yYy0zMy4zZS42MS81Mi8zYS8yNC81MS8yYy8zOC4yMScpLAoJfSwKKQoKCjIzID0gKAoJewoJCSc0JzogJzQ4IDU0IDU3IDRiJywKCQknNyc6ICcyMi44JywKCQknMCc6ICgnNDk6Ly81Yi4xZS4yOS80MS80Ni4yOS8xZC8nCgkJCQkJICAgJzRhLzRmLjVjJyksCgl9LAop")))(lambda a,b:b[int("0x"+a.group(1),16)],"stream_url|news12|afe3010000000000|b8e3010000000000|title|vxedge|fs|logo|jpg|rtmp|Weather|b|bce3010000000000|bae3010000000000|b4e3010000000000|b6e3010000000000|News|b3e3010000000000|12|ace3010000000000|aee3010000000000|b5e3010000000000|net|cdn|cv|id|bbe3010000000000|Connecticut|Westchester|master|githubusercontent|http|Brooklyn|m3u8|Hushamallsports|STATIC_STREAMS2|streams|STATIC_STREAMS|index_1200_av|NEWS|Island|com|Hudson|Valley|iphone|JERSEY|Bronx|xbmcswift2|bloomberg|Bloomberg|abc_live4|streaming|streams2|NASA|akamaihd|Long|playlist|NASA_101|6540154|STRINGS|abcnews|abclive|ustream|Plugin|videos|hmemar|319270|NEW|136330|nasatv|husham|import|Events|https|Lists|Links|30001|30101|30100|event|page|live|uhls|TV|IPTV|cdn3|from|Only|btv|ABC|b9e3010000000000|raw|m3u|i|us|HD|lh|tv|t".split("|")))
+
+
+YOUTUBE_CHANNELS = (
+    {
+        'name': 'WXII 12 NEWS',
+        'logo': 'news12.jpg',
+        'channel_id': 'UC0ZsHHC_frpcqneDpMiteCQ',
+        'user': 'hmemar22',
+    }, 
+	{
+        'name': 'WDEF News 12',
+        'logo': 'news12.jpg',
+        'channel_id': 'UCMp6dYTbPOrKFHMH6rGcp2g',
+        'user': 'hmemar22',
+    }, 
+	{
+        'name': 'twelvedottv',
+        'logo': 'news12.jpg',
+        'channel_id': 'UC_DlTqJhRV-OafjHESJciNw',
+        'user': 'hmemar22',
+    }, 
+	{
+        'name': 'CNN',
+        'logo': 'news12.jpg',
+        'channel_id': 'UCupvZG-5ko_eiXAupbDfxWw',
+        'user': 'hmemar22',
+    }, 
+	{
+        'name': 'CNN Live',
+        'logo': 'news12.jpg',
+        'channel_id': 'UCRrW0ddrbFnJCbyZqHHv4KQ',
+        'user': 'hmemar22',
+    }, 
+	{
+        'name': 'euronews (in English)',
+        'logo': 'news12.jpg',
+        'channel_id': 'UCSrZ3UV4jOidv8ppoVuvW9Q',
+        'user': 'hmemar22',
+    },  
+)
+
+
+
+YOUTUBE_URL ='plugin://plugin.video.youtube/channel/%s/?page=1'
+
+plugin = Plugin()
+
+
+@plugin.route('/')
+def show_root_menu():
+    items = [
+        {'label': _('streams'),
+         'path': plugin.url_for('show_streams')},
+		 {'label': _('Videos'),
+         'path': plugin.url_for('show_channels')},
+
+    ]
+    return plugin.finish(items)
+
+
+@plugin.route('/streams/')
+def show_streams():
+    items = [{
+        'label': stream['title'],
+        'thumbnail': get_logo(stream['logo']),
+        'path': stream['stream_url'],
+        'is_playable': True,
+    } for stream in STATIC_STREAMS]
+    return plugin.finish(items)
+
+	
+	
+
+@plugin.route('/channels/')
+def show_channels():
+    items = [{
+        'label': channel['name'],
+        'thumbnail': get_logo(channel['logo']),
+        'path': YOUTUBE_URL % channel['channel_id'],
+    } for channel in YOUTUBE_CHANNELS]
+    return plugin.finish(items)
+
+def get_logo(logo):
+    addon_id = plugin._addon.getAddonInfo('id')
+    return 'special://home/addons/%s/resources/media/%s' % (addon_id, logo)
+
+
+def _(string_id):
+    if string_id in STRINGS:
+        return plugin.get_string(STRINGS[string_id])
+    else:
+        plugin.log.warning('String is missing: %s' % string_id)
+        return string_id
+
+
+def log(text):
+    plugin.log.info(text)
+
+if __name__ == '__main__':
+    plugin.run()
